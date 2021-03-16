711d3c5809470eabbdf356c8ab424f037b55fc5c
jcgarciaca <julio.jcgc@gmail.com>
2021-03-16 10:52:02 -0500

    light optimization in code

--- count_elements_xml.py
+++ count_elements_xml.py
@@ -1,45 +1,37 @@
 """
 Example usage:
-python count_elements_xml.py /home/msdc/jcgarciaca/projects/thalon/sku_detection
+python count_elements_xml.py /home/maquinavirtualnlp/jcgarciaca/flores/MIA1/golden_dataset/modified/Edtd_Annotations
 """
 
 import numpy as np
 import sys
 import os
-
 from lxml import etree
-
 import xml.etree.ElementTree as ET
-import random
 import operator
 
 
 def main():
-	data_dir = sys.argv[1]
-	annotations_dir = os.path.join(data_dir, "Annotations")
-	xml_list = os.listdir(annotations_dir)
+	annotations_dir = sys.argv[1]
+	xml_list = [xml_file for xml_file in os.listdir(annotations_dir) if xml_file.endswith('.xml')]
 	print('length files:', len(xml_list))
-
-	sku_dict = {}	
+	
+	count_dict = {}	
 	for idx, example_xml in enumerate(xml_list):
-		if example_xml.endswith('.txt'):
-			continue
+		print('{}/{}: {}'.format(idx + 1, len(xml_list), example_xml))
 		path = os.path.join(annotations_dir, example_xml)
-		print('path:', path)
 		tree = ET.parse(path)
 		root = tree.getroot()
 		for elem in tree.iter():
 			if 'object' in elem.tag:
 				for attr in list(elem):
 					if 'name' in attr.tag:
-						if attr.text in sku_dict.keys():
-							sku_dict[attr.text] += 1
-						else:
-							sku_dict[attr.text] = 1
+						count_dict[attr.text] = count_dict.get(attr.text, 0) + 1
 	
-	print(sku_dict)
-	print('------------------------------------')
-	print(sorted(sku_dict.items(), key=operator.itemgetter(1)))
+	print(count_dict)
+	# print('------------------------------------')
+	# print(sorted(sku_dict.items(), key=operator.itemgetter(1)))
+	
 
 if __name__ == "__main__":
 	main()
